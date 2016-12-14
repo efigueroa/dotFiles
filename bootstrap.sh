7e9ef1f26f2e96c028c6ffd022d1be200f813537
Eduardo Figueroa <mr.eduardo.figueroa@gmail.com>
2016-12-14 09:26:10 -0800

    updated bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,6 @@
 #move to home directory
 
 echo "----"
-find . -type f -name ".*" -exec cp {} $HOME/ \;
+find . -type f -name ".*" -exec cp {} $HOME/ \; -exec echo "Copied" {} \;
+echo "----"
 echo "Files moved to home directory, enjoy your workflow."
